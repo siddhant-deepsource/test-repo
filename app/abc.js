461e75c548e6659cf0e2872846153413b1924e8c
sidntrivedi012 <sidntrivedi012@gmail.com>
2020-10-28 14:58:19 +0530

    Updated buggy code for security plugin

--- app/abc.js
+++ app/abc.js
@@ -1,30 +1,4 @@
-function doIf() {
-  if (true) {
-    var build = true;
-  }
+var a = require(\'b\');
 
-  console.log(build);
-}
 
-// hello world
-function doIfElse() {
-  if (true) {
-    var build = true;
-  } else {
-    var build = false;
-  }
-}
-
-function doTryCatch() {
-  try {
-    var build = 1;
-  } catch (e) {
-    var f = build;
-  }
-}
-k.readUInt8(0, false);
-var a = new RegExp(\'ab+c\', \'i\')
-var path = "user input";
-child_process.exec("ls -l " + path, function (err, data) {
-  console.log(data);
-});
+var d = new Buffer(c);
